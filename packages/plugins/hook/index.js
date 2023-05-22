4f8ee7242a044948156f5875436ff413e135c617
weiyafei <2553241022@qq.com>
2023-05-22 22:35:21 +0800

    hooks

---
+++ packages/plugins/hook/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-05-22 22:18:33
+ * @LastEditTime: 2023-05-22 22:32:02
+ * @LastEditors: wsy
+ */
+export default function hooks() {
+  return {
+    name: 'vite-plugin-hooks',
+    options(InputOptions) {
+    },
+    config(config, env) {
+    },
+  }
+}
--- packages/plugins/index.js
+++ packages/plugins/index.js
@@ -2,11 +2,13 @@
  * @Description:
  * @Author: wsy
  * @Date: 2023-05-22 21:22:09
- * @LastEditTime: 2023-05-22 21:30:08
+ * @LastEditTime: 2023-05-22 22:22:41
  * @LastEditors: wsy
  */
 import virtualModule from './virtualModule/index.js'
+import hooks from './hook/index.js'
 
 export {
   virtualModule,
+  hooks,
 }
