05f208cbbf2278b009cd47bb0ae23a02855e48dc
weiyafei <2553241022@qq.com>
2023-05-22 22:34:56 +0800

    config

--- packages/std-01/src/main.js
+++ packages/std-01/src/main.js
@@ -2,14 +2,11 @@
  * @Description:
  * @Author: wsy
  * @Date: 2023-05-22 21:18:16
- * @LastEditTime: 2023-05-22 21:43:30
+ * @LastEditTime: 2023-05-22 22:23:33
  * @LastEditors: wsy
  */
 import { createApp } from 'vue'
 import './style.css'
-import { msg } from 'virtual:my-module'
 import App from './App.vue'
-
-console.log(msg)
 
 createApp(App).mount('#app')
--- packages/std-01/vite.config.js
+++ packages/std-01/vite.config.js
@@ -2,18 +2,19 @@
  * @Description:
  * @Author: wsy
  * @Date: 2023-05-22 21:18:16
- * @LastEditTime: 2023-05-22 21:33:55
+ * @LastEditTime: 2023-05-22 22:27:04
  * @LastEditors: wsy
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Inspect from 'vite-plugin-inspect'
-import { virtualModule } from 'plugins'
+import { hooks } from 'plugins'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     Inspect(),
     vue(),
-    virtualModule(),
+    hooks(),
   ],
 })
